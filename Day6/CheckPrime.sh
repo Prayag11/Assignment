f23cf625df4fdfe28594cac6404fcb6db575d611
Prayag11 <prayagmankar1@gmail.com>
2022-07-14 18:23:20 +0530

    programs for factorial and prime no. are updated.

---
+++ Day6/CheckPrime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Program to check no is prime or not.
+echo -e "Enter Number : \c"
+read n
+for((i=2; i<=$n/2; i++))
+do
+  ans=$(( n%i ))
+  if [ $ans -eq 0 ]
+  then
+    echo "$n is not a prime number."
+    exit 0
+  fi
+done
+echo "$n is a prime number."
